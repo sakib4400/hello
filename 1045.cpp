3ff21f5151e2abbda38d8295cb62bc13d11aeb9e
sakib4400 <sakibshahriaremon@gmail.com>
2023-04-08 14:19:30 +0600

    Triangle Types

---
+++ 1045.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    double a, b, c;
+
+    scanf("%lf%lf%lf", &a, &b, &c);
+    if (a >= b + c || b >= a + c || c >= c + a)
+    {
+        printf("NAO FORMA TRIANGULO\n");
+    }
+    else if ((a * a) == (b * b) + (c * c) || (b * b) == (a * a) + (c * c) || (c * c) == (b * b) + (a * a))
+    {
+        printf("TRIANGULO RETANGULO\n");
+    }
+    else if ((a * a) > (b * b) + (c * c) || (b * b) > (a * a) + (c * c) || (c * c) > (b * b) + (a * a))
+    {
+        printf("TRIANGULO OBTUSANGULO\n");
+    }
+    else if ((a * a) < (b * b) + (c * c) || (b * b) < (a * a) + (c * c) || (c * c) < (b * b) + (a * a))
+    {
+        printf("TRIANGULO ACUTANGULO\n");
+    }
+    if (a == b && a == c)
+    {
+        printf("TRIANGULO EQUILATERO\n");
+    }
+    if ((a == b && a != c) || (a == c && a != b) || (b == c && b != a))
+    {
+        printf("TRIANGULO ISOSCELES\n");
+    }
+
+    return 0;
+}
